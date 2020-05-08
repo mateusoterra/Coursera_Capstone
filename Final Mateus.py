653fd214bb74386fdda6e3153c041214337b9b34
mateusoterra <65024672+mateusoterra@users.noreply.github.com>
2020-05-08 10:14:20 -0300

    Add files via upload

---
+++ Final Mateus.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1>This notebook will be mainly used for the capstone project.</h1>
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
